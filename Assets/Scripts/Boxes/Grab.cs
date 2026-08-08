a1e451ad9674bd404c41c6f658f10cbcd39ce05e
ArianaNChaves <navi.ariana99@gmail.com>
2026-08-08 19:34:02 -0300

    feat:  update grab pkg

--- Assets/Scripts/Boxes/Grab.cs
+++ Assets/Scripts/Boxes/Grab.cs
@@ -3,16 +3,22 @@ using UnityEngine;
 public class Grab : MonoBehaviour
 {
     [SerializeField] private InputReaderDrone inputReader;
+    [SerializeField] private GameObject magnetObject;
     [SerializeField] private LayerMask grabLayer;
     
     private bool canGrab = false;
     private HingeJoint _hingeJoint;
     private Rigidbody _box;
-
+    private LayerMask _magnetLayer;
+    private LayerMask _playerLayer;
+    
+    
     void Start()
     {
         _hingeJoint = this.GetComponent<HingeJoint>();
         inputReader.IsGrabbingEvent += Grabbing;
+        _magnetLayer = LayerMask.NameToLayer("Magnet");
+        _playerLayer = LayerMask.NameToLayer("Player");
     }
     
     private void Grabbing(bool obj)
@@ -20,10 +26,12 @@ public class Grab : MonoBehaviour
         
         if (obj && canGrab && _hingeJoint != null)
         {
+            magnetObject.layer = _playerLayer;
             _hingeJoint.connectedBody = _box;
         }
         else
         {
+            magnetObject.layer = _magnetLayer;
             _hingeJoint.connectedBody = null;
         }
     }
@@ -33,6 +41,7 @@ public class Grab : MonoBehaviour
     {
         if (CompareLayers.CompareLayerAndMask(other.gameObject.layer, grabLayer))
         {
+            
             _box = other.GetComponent<BaseGrabable>().GetRigidbody();
             canGrab = true;
         }
